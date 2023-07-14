ebf9cd60af98732f1ba3bcf547ed637ec96e7b8b
ToddLiao <zxc469469@gmail.com>
2023-07-15 00:42:25 +0800

    feat(client): add order mock data

--- packages/client/src/features/order/order.service.ts
+++ packages/client/src/features/order/order.service.ts
@@ -1,23 +1,47 @@
 import { Order } from '../../lib/types';
 
+const mockOrders: Order[] = [
+  {
+    id: '4370c9c5-3ae5-887e-a5d9-9b2caef0023b',
+    message: '這是醫囑1',
+  },
+  {
+    id: 'b2eddec2-e589-7025-fc70-a797f72319f7',
+    message: '這是醫囑2',
+  },
+  {
+    id: 'b3d3ab0f-fb89-08d4-4d52-5815195e2263',
+    message: '這是醫囑3',
+  },
+];
+
 // mock data
 export const getOrders = (): Promise<Order[]> => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve([
-        {
-          id: '4370c9c5-3ae5-887e-a5d9-9b2caef0023b',
-          message: '這是醫囑1',
-        },
-        {
-          id: 'b2eddec2-e589-7025-fc70-a797f72319f7',
-          message: '這是醫囑2',
-        },
-        {
-          id: 'b3d3ab0f-fb89-08d4-4d52-5815195e2263',
-          message: '這是醫囑3',
-        },
-      ]);
+      resolve(mockOrders);
+    }, 3000);
+  });
+};
+
+export const getOrder = (id: string): Promise<Order> => {
+  console.log('getOrder', id);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const order = mockOrders.find((order) => order.id === id);
+      if (order) {
+        resolve(order);
+      } else {
+        reject(new Error('Order not found'));
+      }
+    }, 3000);
+  });
+};
+
+export const createOrder = (order: Order): Promise<Order> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(order);
     }, 3000);
   });
 };
